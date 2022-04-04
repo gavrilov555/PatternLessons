4184943c16c1cfc00450b8bc4012b8b9585a57eb
gavrilov_v <gavrilov_v92@mail.ru>
2022-04-04 23:30:59 +0500

    simple implementation "Prototype"

---
+++ src/main/java/VersionControl.java
@@ -0,0 +1,13 @@
+public class VersionControl {
+    public static void main(String[] args) {
+
+        Project master = new Project(1, "MyProject", "SourceCode sourceCode = new SourceCode()");
+        System.out.println(master);
+
+        ProjectFactory factory = new ProjectFactory(master);
+        Project cloneProject = factory.cloneProject();
+
+        System.out.println("=========================");
+        System.out.println(cloneProject);
+    }
+}
